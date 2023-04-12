ef33db6cb1ac7031e19d1b1c8ea34fc48d81ee92
bang-star <bangseongmin@naver.com>
2023-04-12 22:17:18 +0900

    #3 - Spring Boot 프로젝트 셋팅
    - gitignore 설정
    - 의존성 추가(초기 필요한 설정에 대해서만)

---
+++ src/main/java/com/example/moduleproject/ModuleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.moduleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleProjectApplication.class, args);
+    }
+
+}
