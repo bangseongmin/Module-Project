f31bebf041f161a3742e96cf726099dc85242aca
bang-star <bangseongmin@naver.com>
2023-04-13 23:31:27 +0900

    #4 - 도메인 설계
    서비스 로직은 아직 구상되지 않았고
    필요한 도메인 구성함.

---
+++ src/main/java/com/example/moduleproject/dto/MemberDto.java
@@ -0,0 +1,28 @@
+package com.example.moduleproject.dto;
+
+import com.example.moduleproject.domain.Member;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+
+@RequiredArgsConstructor
+public record MemberDto(
+        Long idx,
+        String username,
+        String password,
+        String fullname,
+        int age,
+        LocalDate birthday
+) {
+
+    public MemberDto fromEntity(Member member) {
+        return new MemberDto(
+                member.getIdx(),
+                member.getUsername(),
+                member.getPassword(),
+                member.getFullname(),
+                member.getAge(),
+                member.getBirthday()
+        );
+    }
+}
